4adb5794201f8df5d6d7e4bf83d7fe8f964976d0
ACER <ACER@LAPTOP-FQJJHO85>
2022-05-20 20:14:11 +0700

    Add project files.

---
+++ UTS PEMROGRAMAN/Dosen.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTS_PEMROGRAMAN
+{
+   
+    internal class Dosen
+    {
+        public int id { get; set; } 
+        public string nama { get; set; }    
+        public int NIK { get; set; }    
+        public string gender { get; set; }  
+        public string course { get; set; }
+
+        public void Dosenn()
+        {
+            Console.WriteLine(" ID     : {0} ", id); 
+
+            Console.WriteLine(" Nama   : {0} ", nama);
+
+            Console.WriteLine(" NIK    : {0} ", NIK);
+
+            Console.WriteLine(" Gender : {0} ", gender);
+
+            Console.WriteLine(" Course : {0} ", course);
+            Console.WriteLine();
+        }
+
+        public void attDosen()
+           
+        {
+           
+            Console.WriteLine(" Penambahana nama dosen dan NIK dosen  ");
+            Console.Write(" Masukkan Nama = ");
+            string nama = Console.ReadLine();
+            Console.Write(" Masukkan NIK  = ");
+            int NIK=Console.Read();
+            
+
+            }
+            
+        public void addCourse()
+        {
+            Console.WriteLine(" Penambahan Matkul ");
+            Console.Write(" Mata Kuliah   = ");
+            string course = Console.ReadLine();
+            Console.ReadLine();
+            
+        }
+       
+    }
+}
---
+++ UTS PEMROGRAMAN/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTS_PEMROGRAMAN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Dosen dosen = new Dosen();
+
+            //pengesetan nilai properties
+            dosen.id = 1234567 ;
+            dosen.nama = "Kamarudin, M.Kom";
+            dosen.NIK = 2345432;
+            dosen.gender = "Laki-laki";
+            dosen.course = "Pemrograman";
+
+            //pemanggilan method
+            dosen.Dosenn();
+            dosen.attDosen();
+            dosen.addCourse();
+
+            Console.ReadKey();
+        }
+    }
+}
